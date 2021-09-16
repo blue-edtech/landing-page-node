2b705fd520745691050f0ae07037cd307312409d
codethi <docencia.thiago@gmail.com>
2021-09-16 15:30:10 -0300

    menssage pos form

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 const app = express();
 const port = 3000;
+let message = "";
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,16 +12,21 @@ app.use(express.urlencoded());
 app.get("/", (req, res) => {
   const devList = ["Backend", "Frontend", "Fullstack"];
   const analyticsList = ["Engenharia de dados", "Ciência de dados"];
+  setTimeout(() => {
+    message = ""
+  }, 1000)
   res.render("index", {
     titulo: "Blue",
     devList: devList,
     analyticsList: analyticsList,
+    message,
   });
 });
 
 app.post("/subscription", (req, res) => {
   const { nome, email } = req.body;
-  res.send({nome: nome, email: email});
+  message = `Parabéns ${nome}, sua inscrição foi realizada com sucesso! Um e-mail foi enviado para: ${email}`;
+  res.redirect("/");
 });
 
 app.listen(port, () =>
